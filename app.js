52673936cd670c4c9878357296ffd48203f833fd
enriquetoscanoh94 <enriquetoscanoh94@icloud.com>
2026-05-07 22:14:04 +0000

    agregue puebas unitarias

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 
+/////////////////////////////////////////////////////////////////////////////
+
 let oneEuroIs = {
-    "JPY": 156.5, 
-    "USD": 1.07,  
-    "GBP": 0.87,  
+    "JPY": 156.5,
+    "USD": 1.07,
+    "GBP": 0.87,
 }
 
 
@@ -10,22 +12,22 @@ const sum = (a, b) => {
     return a + b;
 }
 
-const fromEuroToDollar = function(valueInEuro) {
+const fromEuroToDollar = function (valueInEuro) {
     let valueInDollar = valueInEuro * 1.07;
     return valueInDollar;
 }
 
-const fromDollarToYen = function(valueInDollar) {
+const fromDollarToYen = function (valueInDollar) {
     let valueInEuro = valueInDollar / 1.07;
     let valueInYen = valueInEuro * 156.5;
     return valueInYen;
 }
 
-const fromYenToPound = function(valueInYen) {
+const fromYenToPound = function (valueInYen) {
     let valueInEuro = valueInYen / 156.5;
     let valueInPound = valueInEuro * 0.87;
     return valueInPound;
 }
 
 
-module.exports = {sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
